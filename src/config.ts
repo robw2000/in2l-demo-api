b20b8e9d26e33fa9bcd6d310011ae0561f09017e
Rob Wilburn <rwilburn@in2l.com>
2021-04-01 14:34:05 -0600

    Fixed yarn server command to pre-build dist and added new items to mock db

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,20 @@ interface IApiConfig {
 const apiConfig: IApiConfig = {
   testData: [
     {
+      id: '8',
+      docType: WIDGET_DOC_TYPE,
+      name: 'Wagging Tail Window Wiper',
+      price: 14.99,
+      availableStock: 38
+    },
+    {
+      id: '23',
+      docType: WIDGET_DOC_TYPE,
+      name: 'Squirrel Feeder',
+      price: 49.99,
+      availableStock: 2
+    },
+    {
       id: '1',
       docType: PROFILE_DOC_TYPE,
       userId: 'user1',
@@ -33,6 +47,34 @@ const apiConfig: IApiConfig = {
       name: 'Perfect Paint Polisher',
       price: 24.99,
       availableStock: 539
+    },
+    {
+      id: '24',
+      docType: WIDGET_DOC_TYPE,
+      name: 'Squirrel Proof Bird Feeder',
+      price: 74.49,
+      availableStock: 56
+    },
+    {
+      id: '25',
+      docType: WIDGET_DOC_TYPE,
+      name: 'Racoon Feeder',
+      price: 74.49,
+      availableStock: 56
+    },
+    {
+      id: '26',
+      docType: WIDGET_DOC_TYPE,
+      name: 'Racoon Trap',
+      price: 109.99,
+      availableStock: 100
+    },
+    {
+      id: '5',
+      docType: PROFILE_DOC_TYPE,
+      userId: 'user3',
+      firstName: 'John',
+      lastName: 'Banner'
     }
   ]
 };
