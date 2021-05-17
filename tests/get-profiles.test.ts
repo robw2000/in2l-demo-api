02bfd8c40f6ffb9121cec11c79b2b80c55e6b505
Ben Prater <prater.ben@me.com>
2021-05-17 00:24:41 -0600

    [TICKET_NUMBER] adding tests to account for auth being required

--- tests/get-profiles.test.ts
+++ tests/get-profiles.test.ts
@@ -6,9 +6,23 @@ describe('Get Profiles', () => {
     app.close(done);
   });
 
-  it('should get an array of profiles', async () => {
-    const res = await request(app).get('/api/profiles');
-    // console.log(res);
+  it('should fail without any authentication', async () => {
+    const res = await request(app)
+      .get('/api/profiles')
+    expect(res.status).toEqual(401);
+  });
+
+  it('should fail with incorrect authentication', async () => {
+    const res = await request(app)
+      .get('/api/profiles')
+      .set('Authorization', 'Bearer evil_token');
+    expect(res.status).toEqual(403);
+  });
+
+  it('should get an array of profiles with proper authentication', async () => {
+    const res = await request(app)
+      .get('/api/profiles')
+      .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyOTkiLCJyb2xlcyI6WyJhZG1pbiIsInVzZXIiXSwiaWF0IjoxNjIxMjMyNDE0fQ.RZbuahhKXZgHo1EF-c5LOwt2gwHxSNu4WCVoZDEC2Zs');
     expect(res.status).toEqual(200);
   });
 });
